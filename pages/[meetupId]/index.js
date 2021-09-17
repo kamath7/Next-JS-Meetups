a64f4617a92eeda8ff3bbb96d613027d8f8bbcc8
kamath7 <adithyakamath96@gmail.com>
2021-09-18 01:38:59 +0530

    using getStaticProps for retrieving single meetup. Currently an error. Will be solved by getStaticPath

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -15,4 +15,21 @@ function MeetUpIdPage() {
     />
   );
 }
+export async function getStaticProps(context) {
+  //fetch for single meetup
+  const meetupId = context.params.meetupId
+  console.log(meetupId)
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://m.media-amazon.com/images/M/MV5BODkyODA3MjMwNF5BMl5BanBnXkFtZTgwOTc3NTU0NjM@._CR289,88,1528,859_UX1248_UY702.jpg",
+        title: "The First Meetup",
+        address: "Random Addrss",
+        description: "Some Description",
+      },
+    },
+  };
+}
 export default MeetUpIdPage;
