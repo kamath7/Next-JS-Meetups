3688cd34e45167a54b15e7a10b5c8f740433b892
kamath7 <adithyakamath96@gmail.com>
2021-09-17 20:57:38 +0530

    link to meetup detail

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,11 +1,16 @@
 //somedomain.com/meetupId
 import { useRouter } from "next/router";
+import React from "react";
 function MeetUpIdPage() {
   const router = useRouter();
   return (
-    <div>
-      <h1>Meetup for {router.query.meetupId}</h1>
-    </div>
+    <React.Fragment>
+      <img src= "https://m.media-amazon.com/images/M/MV5BODkyODA3MjMwNF5BMl5BanBnXkFtZTgwOTc3NTU0NjM@._CR289,88,1528,859_UX1248_UY702.jpg" alt='First meetup'/>
+      <h1>The First Meetup</h1>
+      <address>Random Addrss</address>
+      <p>Some random description</p>
+      
+    </React.Fragment>
   );
 }
 export default MeetUpIdPage;
