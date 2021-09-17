bdb32dae2828d8aec82b40d08cfb70ab5c55851b
kamath7 <adithyakamath96@gmail.com>
2021-09-17 20:54:10 +0530

    programatically navigating

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,12 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
-
+import {useRouter} from 'next/router'
 function MeetupItem(props) {
+const router = useRouter()
+function showDetails(){
+  router.push(`/${props.id}`) //to navigate programatically. alternative to link
+}
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +18,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetails}>Show Details</button>
         </div>
       </Card>
     </li>
